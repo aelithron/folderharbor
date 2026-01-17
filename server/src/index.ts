14bd37bb05867976184dd4ae259120172f4659f7
Nova <aelithron@gmail.com>
2026-01-16 20:35:55 -0700

    add better path resolution and config handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
 import { program } from "commander";
 import type z from "zod";
+import path from "path";
 import getConfig, { Config } from "./utils/config.js";
 
 let config: z.Infer<typeof Config> | null = null;
 program
   .name("folderharbor").description("A powerful file server that supports many protocols")
-  .option("-c, --config", "path to server config");
+  .option("-c, --config <path>", "path to server config");
 async function startServer() {
   await program.parseAsync();
-  console.log(`Starting FolderHarbor...`);
-  config = await getConfig(program.opts().config as string | undefined);
+  console.log(`Starting FolderHarbor!`);
+  config = await getConfig((program.opts().config as string | undefined) ? path.resolve(program.opts().config as string) : undefined);
 
 }
 await startServer();
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,13 +1,23 @@
 import * as z from "zod";
 import path from "path";
 import fs from "fs/promises";
+import { fileURLToPath } from "url";
 
 export const Config = z.object({
   database: z.string()
 });
 export default async function getConfig(configPath?: string): Promise<z.Infer<typeof Config>> {
   if (!configPath) configPath = "/etc/folderharbor/config.json";
-  // if (!fs.existsSync(configPath)) {}
+  try {
+    await fs.access(configPath, fs.constants.R_OK);
+  } catch {
+    if (configPath === "/etc/folderharbor/config.json") {
+      console.warn("Default config is missing, copying...");
+      await fs.copyFile(path.join(path.dirname(fileURLToPath(import.meta.url)), "../../example.config.json"), configPath, fs.constants.COPYFILE_EXCL);
+    } else {
+      throw new Error("Location couldn't be found!");
+    }
+  }
   const configFile = await fs.readFile(configPath, "utf8");
   try {
     return Config.parse(JSON.parse(configFile));
