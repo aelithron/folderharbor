5ff42361f7ba0150e52840f9c9348dd224f1470b
Nova <aelithron@gmail.com>
2026-01-18 22:00:00 -0700

    check for overly permissive config, improve error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,15 +7,21 @@ let config: z.Infer<typeof Config> | null = null;
 program
   .name("folderharbor").description("A powerful file server that supports many protocols")
   .option("-c, --config <path>", "path to server config")
-  .option("--allow-root-user", "allow the server to run as root (this can cause security risks!)");
+  .option("--allow-root-user", "allow the server to run as root (this can cause security risks!)")
+  .option("--allow-permissive-config", "allow the server to load a configuration that is overly permissive");
 async function startServer() {
   await program.parseAsync();
   if ((process.getuid && process.getgid) && (process.getuid() === 0 || process.getgid() === 0) && !program.opts().allowRootUser) {
-    console.error('You started FolderHarbor as root! This is a security risk.\nIf you understand the risks, you can re-invoke the server with the "--allow-root-user" argument.');
+    console.error('You started FolderHarbor as root! This is a security risk.\nIf you understand the risks, you can override this check with the "--allow-root-user" argument.');
     process.exit(1);
   }
   console.log(`Starting FolderHarbor!`);
-  config = await getConfig((program.opts().config as string | undefined) ? path.resolve(program.opts().config as string) : undefined);
-  if (config.database === "") throw new Error("Missing or invalid database credentials!");
+  try {
+    config = await getConfig(program.opts().allowPermissiveConfig, (program.opts().config as string | undefined) ? path.resolve(program.opts().config as string) : undefined);
+  } catch (e) {
+    console.error(`Config Error - ${e}`);
+    process.exit(1);
+  }
+  console.log("FolderHarbor ready!");
 }
 await startServer();
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -4,9 +4,9 @@ import fs from "fs/promises";
 import { fileURLToPath } from "url";
 
 export const Config = z.object({
-  database: z.string()
+  database: z.string().nonempty()
 });
-export default async function getConfig(configPath?: string): Promise<z.Infer<typeof Config>> {
+export default async function getConfig(allowPermissive: boolean, configPath?: string): Promise<z.Infer<typeof Config>> {
   if (!configPath) configPath = "/etc/folderharbor/config.json";
   try {
     await fs.access(configPath, fs.constants.R_OK);
@@ -15,14 +15,20 @@ export default async function getConfig(configPath?: string): Promise<z.Infer<ty
       console.warn("Default config is missing, copying...");
       await fs.mkdir(path.dirname(configPath), { recursive: true });
       await fs.copyFile(path.join(path.dirname(fileURLToPath(import.meta.url)), "../../example.config.json"), configPath, fs.constants.COPYFILE_EXCL);
+      if (process.getuid && process.getgid) await fs.chown(configPath, process.getuid(), process.getgid());
+      await fs.chmod(configPath, 0o600);
     } else {
       throw new Error("Passed config path couldn't be found!");
     }
+  }
+  if (!allowPermissive && process.getuid) {
+    const info = await fs.stat(configPath);
+    if (info.uid !== process.getuid() || (info.mode & 0o777) !== 0o600) throw new Error(`The config file is overly permissive, meaning it could be read or modified by another user.\nTo fix this, set chmod 600 on the config and change the owner to this user (user ID ${process.getuid()}).\nAlternatively, if you know what you're doing, you can override this check with the "--allow-permissive-config" argument.`);
   }
   const configFile = await fs.readFile(configPath, "utf8");
   try {
     return Config.parse(JSON.parse(configFile));
   } catch (e) {
-    throw new Error(`Config file was incorrectly formatted! \n${e}`);
+    throw new Error(`The config file was incorrectly formatted!\n${e}`);
   }
 }
