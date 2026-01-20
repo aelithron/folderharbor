ec8bb75ec26b010c012c69e51ebcc9a3d5194ff9
Nova <aelithron@gmail.com>
2026-01-19 22:09:21 -0700

    fix server start errors and improve assorted dev stuff

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import type { Server } from "http";
-import baseRouter from "./base.js";
+import { router as utilsRouter } from "./utils.js";
 export default async function startAPI(port: number): Promise<Server> {
   const app = express();
   app.get("/", (req, res) => res.json({ server: "FolderHarbor" }));
-  app.use("/base", baseRouter);
-  return app.listen(port, () => console.log(`API server running (port ${port})`));
+  app.use("/utils", utilsRouter);
+  const server = app.listen(port);
+  server.on("listening", () => console.log(`API server running (port ${port})`));
+  return server;
 }
--- server/src/api/base.ts
+++
@@ -1,4 +0,0 @@
-import express, { Router } from "express";
-const route: Router = express.Router();
-route.get("/", (req, res) => res.send("idk even anymore, tired :c"));
-export default route;
