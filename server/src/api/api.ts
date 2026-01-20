158e850ce997935b7425f821be0e98bc74538cc5
Nova <aelithron@gmail.com>
2026-01-19 21:37:50 -0700

    small amount of api work

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import type { Server } from "http";
+import baseRouter from "./base.js";
 export default async function startAPI(port: number): Promise<Server> {
   const app = express();
-  app.get("/", (req, res) => res.send("FolderHarbor API!"));
+  app.get("/", (req, res) => res.json({ server: "FolderHarbor" }));
+  app.use("/base", baseRouter);
   return app.listen(port, () => console.log(`API server running (port ${port})`));
 }
