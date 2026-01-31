bce54f1f4465cd0cfbb5c9c0b205d782eb2b7576
Nova <aelithron@gmail.com>
2026-01-31 00:59:26 -0700

    permission things :3

--- server/src/permissions/permissions.ts
+++ server/src/permissions/permissions.ts
@@ -1,3 +1,9 @@
-export const permissions: { id: `${string}:${string}`, description: string }[] = [
-  { id: "", description: "" }
+export const permissions: { id: `${"users" | "sessions" | "roles" | "acls" | "config"}:${string}`, description: string }[] = [
+  { id: "users:create", description: "Create new users" },
+  { id: "users:read", description: "Read users' full information" },
+  { id: "users:edit", description: "Modify the information of other users" },
+  { id: "users:delete", description: "Delete users" },
+  { id: "users:list", description: "Get a list of all users, with limited information" },
+  
+  { id: "", description: "" },
 ];
