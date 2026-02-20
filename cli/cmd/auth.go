1d064be09907f9f406a3a2b558a4abfb3ed382bd
Nova <aelithron@gmail.com>
2026-02-20 17:38:56 +0000

    add a login command in full

--- cli/cmd/auth.go
+++ cli/cmd/auth.go
@@ -1,1 +1,35 @@
 package cmd
+
+import (
+	"FolderHarbor-CLI/routes"
+	"bufio"
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var authCMD = &cobra.Command{
+	Use:   "auth",
+	Short: "authenticate with a folderharbor server",
+	Long:  "manage authentication with a folderharbor server",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+var loginCMD = &cobra.Command{
+	Use:   "login",
+	Short: "log into a folderharbor server",
+	Long:  "log into a folderharbor server",
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter your username: ")
+		username, _ := reader.ReadString('\n')
+		fmt.Print("Enter your username: ")
+		password, _ := reader.ReadString('\n')
+		fmt.Print(routes.Login(username, password))
+	},
+}
+func init() {
+	rootCMD.AddCommand(authCMD)
+	authCMD.AddCommand(loginCMD)
+}
--- cli/routes/auth.go
+++ cli/routes/auth.go
@@ -1,11 +1,24 @@
 package routes
+
 import (
-  "net/http"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
 )
 
-func Login() {
-	resp, err := http.Post("http://localhost:3000/auth")
+type login struct {
+	Username string `json:"username"`
+	Password string `json:"password"` 
+}
+func Login(username, password string) (map[string]any) {
+	reqBody, _ := json.Marshal(&login{ Username: username, Password: password })
+	resp, err := http.Post("http://localhost:3000/auth", "application/json", bytes.NewBuffer(reqBody))
 	if err != nil { panic (err) }
 	defer resp.Body.Close()
-	
+	resBody, err := io.ReadAll(resp.Body)
+	if err != nil { panic (err) }
+	var body map[string]any
+	if err := json.Unmarshal(resBody, &body); err != nil { panic (err) }
+	return body
 }
