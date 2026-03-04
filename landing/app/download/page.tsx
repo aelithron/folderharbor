9fb48563570abc8ef95f8d671d3ab5a0d80324e8
Nova <aelithron@gmail.com>
2026-03-04 16:57:42 +0000

    landing: add cli and web panel to download page

--- landing/app/download/page.tsx
+++ landing/app/download/page.tsx
@@ -1,14 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faServer } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faServer, faTerminal, faGlobe } from '@fortawesome/free-solid-svg-icons'
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-8 md:p-20">
       <h1 className="text-3xl font-semibold text-center"><FontAwesomeIcon icon={faDownload} /> Download FolderHarbor</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
         <div className="flex flex-col gap-2 items-center text-center">
           <FontAwesomeIcon icon={faServer} size="xl" />
           <h2 className="text-xl font-semibold">Server</h2>
-          <a href="https://github.com/aelithron/folderharbor" className="hover:text-sky-500 bg-violet-500 p-2 rounded-xl"><FontAwesomeIcon icon={faDownload} /> Download</a>
+          <a href="https://github.com/aelithron/folderharbor" target="_blank" className="hover:text-sky-500 bg-violet-500 p-2 rounded-xl"><FontAwesomeIcon icon={faDownload} /> Download</a>
+        </div>
+        <div className="flex flex-col gap-2 items-center text-center">
+          <FontAwesomeIcon icon={faTerminal} size="xl" />
+          <h2 className="text-xl font-semibold">CLI</h2>
+          <a href="https://github.com/aelithron/folderharbor" target="_blank" className="hover:text-sky-500 bg-violet-500 p-2 rounded-xl"><FontAwesomeIcon icon={faDownload} /> Download</a>
+        </div>
+        <div className="flex flex-col gap-2 items-center text-center">
+          <FontAwesomeIcon icon={faGlobe} size="xl" />
+          <h2 className="text-xl font-semibold">Web Panel</h2>
+          <a href="https://github.com/aelithron/folderharbor" target="_blank" className="hover:text-sky-500 bg-violet-500 p-2 rounded-xl"><FontAwesomeIcon icon={faDownload} /> Download</a>
         </div>
       </div>
     </main>
