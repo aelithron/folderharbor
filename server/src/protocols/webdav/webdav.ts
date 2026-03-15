8a419fba13431ea4752d46d44bc7f4e2ec77442b
Nova <aelithron@gmail.com>
2026-03-14 23:59:47 -0600

    server: remove the tutorial logging code i added some logging code from the tutorial, so removing that now

--- server/src/protocols/webdav/webdav.ts
+++ server/src/protocols/webdav/webdav.ts
@@ -4,12 +4,5 @@ export default async function startWebDAV(port: number): Promise<webdav.WebDAVSe
   const server = new webdav.WebDAVServer({ port, requireAuthentification: true, httpAuthentication: new webdav.HTTPBasicAuthentication(new FolderHarborUserManager(), "FolderHarbor"), privilegeManager: new FolderHarborPrivilegeManager(), serverName: "FolderHarbor WebDAV" });
   await server.setFileSystemAsync("/", new webdav.PhysicalFileSystem("/"));
   server.start(() => console.log(`WebDAV server running (port ${port})`));
-  server.afterRequest((arg, next) => {
-    // Display the method, the URI, the returned status code and the returned message
-    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
-    // If available, display the body of the response
-    console.log(arg.responseBody);
-    next();
-  });
   return server;
 }
