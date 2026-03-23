ca064da9f8f29258a76e13494be8101335965608
Nova <aelithron@gmail.com>
2026-03-23 08:27:46 -0600

    server: add user granting api, remove user granting from edit

--- server/src/api/admin/users.ts
+++ server/src/api/admin/users.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { checkPermission, permissions } from "../../permissions/permissions.js";
+import { checkPermission, getPermissionIDs } from "../../permissions/permissions.js";
 import { createUser, deleteUser, editUser, getAllUsers, getUser } from "../../users/users.js";
 import { getUserSessions } from "../../users/sessions.js";
 import { getAllACLs } from "../../permissions/acls.js";
@@ -32,7 +32,7 @@ router.post("/", async (req, res) => {
   if (!req.body) return res.status(400).json({ error: "request_body", message: "Your request's body is empty or invalid." });
   if (!req.body.username || (req.body.username as string).length < 1) return res.status(400).json({ error: "username", message: 'No username ("username" parameter) provided.' });
   if (!req.body.password || (req.body.password as string).length < 1) return res.status(400).json({ error: "password", message: 'No password ("password" parameter) provided.' });
-  const newUser = await createUser(req.body.username, req.body.password, {  });
+  const newUser = await createUser(req.body.username, req.body.password, {});
   if ("error" in newUser) {
     switch (newUser.error) {
       case "server":
@@ -89,50 +89,12 @@ router.patch("/:userID", async (req, res) => {
     console.error(`Server Error - Couldn't read session in an auth-enforced route!\nPath: ${req.originalUrl}\nMethod: ${req.method}`);
     return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
   }
-  let accessLevel: "full" | "limited" | null = null;
-  if (await checkPermission(req.session.userID, "users:edit.full")) {
-    accessLevel = "full";
-  } else if (await checkPermission(req.session.userID, "users:edit")) {
-    accessLevel = "limited";
-  } else return res.status(403).json({ error: "forbidden", message: "You don't have permission to do this!" });
+  if (!await checkPermission(req.session.userID, "users:edit") && !await checkPermission(req.session.userID, "users:edit.full")) return res.status(403).json({ error: "forbidden", message: "You don't have permission to do this!" });
   if (!req.body) return res.status(400).json({ error: "request_body", message: "Your request's body is empty or invalid." });
-  let result;
-  let updateParams: Partial<{ username: string, password: string, clearLoginAttempts: boolean, roles: number[], acls: number[], permissions: (`users:${string}` | `roles:${string}` | `acls:${string}` | `config:${string}`)[] }> = {};
-  if (accessLevel === "full") {
-    if (Array.isArray(req.body.permissions)) for (const node of req.body.permissions) if (!permissions.find(otherNode => otherNode.id === node)) return res.status(400).json({ error: "permissions", message: `Permission "${node}" doesn't exist, please correct this and try again.` });
-    if (Array.isArray(req.body.acls)) {
-      const acls = await getAllACLs();
-      if ("error" in acls) {
-        switch (acls.error) {
-          case "server":
-            return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
-          default:
-            return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
-        }
-      }
-      for (const id of req.body.acls) if (!acls.find(acl => acl.id === id)) return res.status(400).json({ error: "acls", message: `ACL "${id}" doesn't exist, please correct this and try again.` });
-    }
-    if (Array.isArray(req.body.roles)) {
-      const roles = await getAllRoles();
-      if ("error" in roles) {
-        switch (roles.error) {
-          case "server":
-            return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
-          default:
-            return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
-        }
-      }
-      for (const id of req.body.roles) if (!roles.find(role => role.id === id)) return res.status(400).json({ error: "roles", message: `Role "${id}" doesn't exist, please correct this and try again.` });
-    }
-    updateParams = { username: req.body.username, password: req.body.password, clearLoginAttempts: req.body.clearLoginAttempts, roles: (Array.isArray(req.body.roles) ? req.body.roles : undefined), acls: (Array.isArray(req.body.acls) ? req.body.acls : undefined), permissions: (Array.isArray(req.body.permissions) ? req.body.permissions : undefined) };
-    if (Object.values(updateParams).filter((value) => value !== undefined).length === 0) return res.json({ success: true, message: "Nothing to update." });
-    result = await editUser(parseInt(req.params.userID), updateParams);
-  }
-  if (accessLevel === "limited") {
-    updateParams = { username: req.body.username, password: req.body.password, clearLoginAttempts: req.body.clearLoginAttempts };
-    if (Object.values(updateParams).filter((value) => value !== undefined).length === 0) return res.json({ success: true, message: "Nothing to update." });
-    result = await editUser(parseInt(req.params.userID), updateParams);
-  }
+  let updateParams: Partial<{ username: string, password: string, clearLoginAttempts: boolean }> = {};
+  updateParams = { username: req.body.username, password: req.body.password, clearLoginAttempts: req.body.clearLoginAttempts };
+  if (Object.values(updateParams).filter((value) => value !== undefined).length === 0) return res.json({ success: true, message: "Nothing to update." });
+  const result = await editUser(parseInt(req.params.userID), updateParams);
   if (result === undefined) return res.status(500).json({ error: "unknown", message: "An unknown error occurred." });
   if ("error" in result) {
     switch (result.error) {
@@ -147,7 +109,7 @@ router.patch("/:userID", async (req, res) => {
     }
   }
   if (updateParams.password) updateParams.password = "[redacted]";
-  await writeLog(req.session.userID, req.session.username, "users-edit", { id: parseInt(req.params.userID), accessLevel, newContents: updateParams }, "edited a user");
+  await writeLog(req.session.userID, req.session.username, "users-edit", { id: parseInt(req.params.userID), newContents: updateParams }, "edited a user");
   return res.json({ success: true });
 });
 router.patch("/:userID/lock", async (req, res) => {
@@ -171,6 +133,91 @@ router.patch("/:userID/lock", async (req, res) => {
     }
   }
   await writeLog(req.session.userID, req.session.username, "users-edit", { id: parseInt(req.params.userID), newContents: { locked: req.body.locked } }, "edited a user");
+  return res.json({ success: true });
+});
+router.patch("/:userID/grant/:type", async (req, res) => {
+  if (!req.session) {
+    console.error(`Server Error - Couldn't read session in an auth-enforced route!\nPath: ${req.originalUrl}\nMethod: ${req.method}`);
+    return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
+  }
+  if (!await checkPermission(req.session.userID, "users:edit.full")) return res.status(403).json({ error: "forbidden", message: "You don't have permission to do this!" });
+  if (!req.body) return res.status(400).json({ error: "request_body", message: "Your request's body is empty or invalid." });
+  const updateParams: Partial<{ roles: number[], acls: number[], permissions: (`users:${string}` | `roles:${string}` | `acls:${string}` | `config:${string}` | `logs:${string}`)[] }> = {};
+  const user = await getUser(req.session.userID);
+  if ("error" in user) {
+    switch (user.error) {
+      case "server":
+        return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
+      case "not_found":
+        return res.status(400).json({ error: "not_found", message: "Error looking up your session, please sign in again." });
+      default:
+        return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
+    }
+  }
+  switch (req.params.type) {
+    case "roles": {
+      const roles = new Set<number>(user.roles);
+      if (!req.body.roles || !Array.isArray(req.body.roles)) return res.status(400).json({ error: "roles", message: `Your "roles" array is missing/malformed!` });
+      const allRoles = await getAllRoles();
+      if ("error" in allRoles) {
+        switch (allRoles.error) {
+          case "server":
+            return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
+          default:
+            return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
+        }
+      }
+      for (const item of (req.body.roles as number[])) {
+        for (const id of req.body.acls) if (!allRoles.find(acl => acl.id === id)) return res.status(400).json({ error: "roles", message: `Role "${id}" doesn't exist, please correct this and try again.` });
+        roles.add(item);
+      }
+      updateParams.roles = [...roles];
+      break;
+    }
+    case "acls": {
+      const acls = new Set<number>(user.acls);
+      if (!req.body.acls || !Array.isArray(req.body.acls)) return res.status(400).json({ error: "acls", message: `Your "acls" array is missing/malformed!` });
+      const allACLs = await getAllACLs();
+      if ("error" in allACLs) {
+        switch (allACLs.error) {
+          case "server":
+            return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
+          default:
+            return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
+        }
+      }
+      for (const item of (req.body.acls as number[])) {
+        for (const id of req.body.acls) if (!allACLs.find(acl => acl.id === id)) return res.status(400).json({ error: "acls", message: `ACL "${id}" doesn't exist, please correct this and try again.` });
+        acls.add(item);
+      }
+      updateParams.acls = [...acls];
+      break;
+    }
+    case "permissions": {
+      const permissions = new Set<`users:${string}` | `roles:${string}` | `acls:${string}` | `config:${string}` | `logs:${string}`>(user.permissions);
+      if (!req.body.permissions || !Array.isArray(req.body.permissions)) return res.status(400).json({ error: "permissions", message: `Your "permissions" array is missing/malformed!` });
+      for (const item of (req.body.permissions as (`users:${string}` | `roles:${string}` | `acls:${string}` | `config:${string}` | `logs:${string}`)[])) {
+        if (!getPermissionIDs().includes(item)) return res.status(400).json({ error: "permissions", message: `Permission "${item}" doesn't exist, please correct this and try again.` });
+        permissions.add(item);
+      }
+      updateParams.permissions = [...permissions];
+      break;
+    }
+    default:
+      return res.status(400).json({ error: "type", message: "That item type doesn't exist, or isn't grantable!" });
+  }
+  const result = await editUser(parseInt(req.params.userID), updateParams);
+  if ("error" in result) {
+    switch (result.error) {
+      case "server":
+        return res.status(500).json({ error: "server", message: "Something went wrong on the server's end, please contact your administrator." });
+      case "not_found":
+        return res.status(400).json({ error: "not_found", message: "The provided user doesn't exist." });
+      default:
+        return res.status(500).json({ error: "unknown", message: "An unknown error occured." });
+    }
+  }
+  await writeLog(req.session.userID, req.session.username, "users-edit", { id: parseInt(req.params.userID), newContents: updateParams }, "edited a user");
   return res.json({ success: true });
 });
 router.delete("/:userID", async (req, res) => {
