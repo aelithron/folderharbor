045da348cc097eaa221036d3b265558fef12ea52
Nova <aelithron@gmail.com>
2026-03-23 02:37:31 -0600

    cli: add more role management routes and commands

--- cli/cmd/roles.go
+++ cli/cmd/roles.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"folderharbor-cli/routes"
+	"os"
+	"slices"
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -24,7 +29,68 @@ var listRolesCMD = &cobra.Command{
 		for role := range roles { fmt.Printf("• %s (ID %d)\n", roles[role].Name, roles[role].RoleID) }
 	},
 }
+var getRoleCMD = &cobra.Command{
+	Use: "get <id>",
+	Short: "get info on a role",
+	Long: "get information about a specific role",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		roleID, err := strconv.Atoi(args[0])
+		if err != nil { panic (err) }
+		info := routes.GetRole(roleID)
+		fmt.Println("Information for role " + info.Name + " (ID " + fmt.Sprint(roleID) + ")")
+		fmt.Println("----------------------------")
+		fmt.Printf("ACLs: ")
+		if len(info.ACLs) == 0 { 
+			fmt.Println("None") 
+		} else {
+			fmt.Println()
+			for acl := range info.ACLs { fmt.Println("• #" + fmt.Sprint(info.ACLs[acl])) }
+		}
+		fmt.Printf("Permissions: ")
+		if len(info.Permissions) == 0 { 
+			fmt.Println("None") 
+		} else {
+			fmt.Println()
+			slices.Sort(info.Permissions)
+			for permission := range info.Permissions { fmt.Println("• " + info.Permissions[permission]) }
+		}
+	},
+}
+var createRoleCMD = &cobra.Command{
+	Use:   "create",
+	Short: "create a role",
+	Long:  "create a new role",
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter the new role's name: ")
+		name, _ := reader.ReadString('\n')
+		roleID := routes.CreateRole(strings.TrimSpace(name))
+		fmt.Println("New role created! (ID: #" + fmt.Sprint(roleID) + ")")
+	},
+}
+var deleteRoleCMD = &cobra.Command{
+	Use: "delete <id>",
+	Short: "delete a role",
+	Long: "permanently delete a specific role",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		roleID, err := strconv.Atoi(args[0])
+		if err != nil { panic (err) }
+		routes.DeleteRole(roleID)
+		fmt.Println("Successfully deleted the role (ID: #" + fmt.Sprint(roleID) + ").")
+	},
+}
+var updateRoleCMD = &cobra.Command{
+	Use: "update",
+	Short: "update a role's info",
+	Long: "update information for a role",
+}
 func init() {
   rootCMD.AddCommand(rolesCMD)
 	rolesCMD.AddCommand(listRolesCMD)
+	rolesCMD.AddCommand(getRoleCMD)
+	rolesCMD.AddCommand(createRoleCMD)
+	rolesCMD.AddCommand(deleteRoleCMD)
+	rolesCMD.AddCommand(updateRoleCMD)
 }
--- cli/routes/roles.go
+++ cli/routes/roles.go
@@ -1,6 +1,9 @@
 package routes
+
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -10,6 +13,11 @@ import (
 type RoleList struct { 
 	RoleID int `json:"id"`
 	Name string `json:"name"`
+}
+type Role struct {
+	Name string `json:"name"`
+	Permissions []string `json:"permissions"`
+	ACLs []int `json:"acls"`
 }
 
 func ListRoles() ([]RoleList) {
@@ -35,4 +43,99 @@ func ListRoles() ([]RoleList) {
 	var body []RoleList
 	if err := json.Unmarshal(resBody, &body); err != nil { panic (err) }
 	return body	
+}
+type createRoleReq struct { Name string `json:"string"` }
+type createRoleRes struct { ID int `json:"id"` }
+func CreateRole(name string) (int) {
+	auth := getAuth()
+	reqBody, _ := json.Marshal(&createRoleReq{ Name: name })
+	addr, err := url.Parse(auth.Server)
+	if err != nil { panic (err) }
+	addr.Path = path.Join(addr.Path, "/admin/roles")
+	req, err := http.NewRequest(http.MethodPost, addr.String(), bytes.NewBuffer(reqBody))
+	cookie := http.Cookie{ Name: "token", Value: auth.Token, Path: "/" }
+	req.AddCookie(&cookie)
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil { panic (err) }
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil { panic (err) }
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil { panic (err) }
+	if res.StatusCode != 200 {
+		var errBody APIError
+		if err := json.Unmarshal(resBody, &errBody); err != nil { panic (err) }
+		if errBody.Error != "" { handleAPIError(errBody) }
+	}
+	var body createRoleRes
+	if err := json.Unmarshal(resBody, &body); err != nil { panic (err) }
+	return body.ID
+}
+func GetRole(roleID int) (Role) {
+	auth := getAuth()
+	addr, err := url.Parse(auth.Server)
+	if err != nil { panic (err) }
+	addr.Path = path.Join(addr.Path, "/admin/roles/" + fmt.Sprint(roleID))
+	req, err := http.NewRequest(http.MethodGet, addr.String(), nil)
+	cookie := http.Cookie{ Name: "token", Value: auth.Token, Path: "/" }
+	req.AddCookie(&cookie)
+	if err != nil { panic (err) }
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil { panic (err) }
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil { panic (err) }
+	var errBody APIError
+	if err := json.Unmarshal(resBody, &errBody); err != nil { panic (err) }
+	if errBody.Error != "" { handleAPIError(errBody) }
+	var body Role
+	if err := json.Unmarshal(resBody, &body); err != nil { panic (err) }
+	return body
+}
+type RoleInfoWrite struct {
+	Name string `json:"name,omitempty"`
+	Permissions []string `json:"permissions,omitempty"`
+	ACLs []int `json:"acls,omitempty"`
+}
+func UpdateRole(roleID int, info RoleInfoWrite) {
+	auth := getAuth()
+	reqBody, _ := json.Marshal(info)
+	addr, err := url.Parse(auth.Server)
+	if err != nil { panic (err) }
+	addr.Path = path.Join(addr.Path, "/admin/roles/" + fmt.Sprint(roleID))
+	req, err := http.NewRequest(http.MethodPatch, addr.String(), bytes.NewBuffer(reqBody))
+	cookie := http.Cookie{ Name: "token", Value: auth.Token, Path: "/" }
+	req.AddCookie(&cookie)
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil { panic (err) }
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil { panic (err) }
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil { panic (err) }
+	var errBody APIError
+	if err := json.Unmarshal(resBody, &errBody); err != nil { panic (err) }
+	if errBody.Error != "" { handleAPIError(errBody) }
+}
+func DeleteRole(roleID int) {
+	auth := getAuth()
+	addr, err := url.Parse(auth.Server)
+	if err != nil { panic (err) }
+	addr.Path = path.Join(addr.Path, "/admin/roles/" + fmt.Sprint(roleID))
+	req, err := http.NewRequest(http.MethodDelete, addr.String(), nil)
+	cookie := http.Cookie{ Name: "token", Value: auth.Token, Path: "/" }
+	req.AddCookie(&cookie)
+	if err != nil { panic (err) }
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil { panic (err) }
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil { panic (err) }
+	var errBody APIError
+	if err := json.Unmarshal(resBody, &errBody); err != nil { panic (err) }
+	if errBody.Error != "" { handleAPIError(errBody) }
 }
