35bdeb84c7dfe1798aa0955906b615a53331a2b4
Nova <aelithron@gmail.com>
2026-03-23 06:19:47 -0600

    cli: add log reading command and route

---
+++ cli/cmd/tools.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"folderharbor-cli/routes"
+	"strconv"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var toolsCMD = &cobra.Command{
+	Use:   "tools",
+	Aliases: []string{"utils"},
+	Short: "some extra tools",
+	Long:  "extra tools for folderharbor",
+}
+var readLogsCMD = &cobra.Command{
+	Use: "logs [page]",
+	Short: "(admin) read the audit log",
+	Long: "(admin) read the server's audit log",
+	Args: cobra.RangeArgs(0, 1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var page = 1
+		if len(args) == 1 {
+			pageArg, err := strconv.Atoi(args[0])
+			if err != nil { panic (err) }
+			page = pageArg
+		}
+		logs := routes.ReadLogs(page)
+		fmt.Println("Audit Logs (Page " + fmt.Sprint(page) + "/" + fmt.Sprint(logs.PageCount) + ")")
+		fmt.Println("----------------------------")
+    if len(logs.Logs) == 0 { fmt.Println("No logs on this page!") }
+	  for log := range logs.Logs { fmt.Println(logFormatter(logs.Logs[log])) }
+	},
+}
+func init() {
+	rootCMD.AddCommand(toolsCMD)
+	toolsCMD.AddCommand(readLogsCMD)
+}
+func logFormatter(log routes.LogEntry) (string) {
+	if log.Username == "" { log.Username = "Unknown User" }
+	createdAtRaw, err := time.Parse(time.RFC3339, log.CreatedAt)
+	var createdAt string
+	if err == nil {
+		createdAt = createdAtRaw.Format(time.RFC1123)
+	} else {
+		createdAt = log.CreatedAt
+	}
+	var data map[string]any
+	if err := json.Unmarshal(log.Body, &data); err != nil { panic (err) }
+  var line1 = "• " + log.Username + " (ID #" + fmt.Sprint(log.UserID) + ") " + log.Blurb + " on " + createdAt + " (action: " + log.Action + ")"
+	return line1 + "\n  " + fmt.Sprint(data)
+}
---
+++ cli/routes/tools.go
@@ -0,0 +1,50 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+type AuditLog struct {
+	PageCount int `json:"pageCount"`
+	Logs []LogEntry `json:"logs"`
+}
+type LogEntry struct {
+	UserID int `json:"userID"`
+	Username string `json:"username"`
+	Action string `json:"action"`
+	Body json.RawMessage `json:"body"`
+  Blurb string `json:"blurb"`
+	CreatedAt string `json:"createdAt"`
+}
+func ReadLogs(page int) (AuditLog) {
+	auth := getAuth()
+	addr, err := url.Parse(auth.Server)
+	if err != nil { panic (err) }
+	addr.Path = path.Join(addr.Path, "/admin/logs")
+	query := addr.Query()
+	query.Add("page", fmt.Sprint(page))
+	addr.RawQuery = query.Encode()
+	req, err := http.NewRequest(http.MethodGet, addr.String(), nil)
+	cookie := http.Cookie{ Name: "token", Value: auth.Token, Path: "/" }
+	req.AddCookie(&cookie)
+	if err != nil { panic (err) }
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil { panic (err) }
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil { panic (err) }
+	if res.StatusCode != 200 {
+		var errBody APIError
+		if err := json.Unmarshal(resBody, &errBody); err != nil { panic (err) }
+		if errBody.Error != "" { handleAPIError(errBody) }
+	}
+	var body AuditLog
+	if err := json.Unmarshal(resBody, &body); err != nil { panic (err) }
+	return body
+}
