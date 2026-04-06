bd14f07f46138fcfee323fd09e8c09e8bef883c7
Nova <aelithron@gmail.com>
2026-04-06 05:15:28 -0600

    web: small fetch wrapper, added infra for platform info

--- web/app/home/home.module.tsx
+++ web/app/home/home.module.tsx
@@ -1,17 +1,36 @@
 "use client"
 import { Session } from "@/folderharborweb";
+import query from "@/utils/api";
 import { db } from "@/utils/db";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [session, setSession] = useState<Session | undefined>(undefined);
+  const [session, setSession] = useState<Session | undefined>();
+  const [webdavURL, setWebdavURL] = useState<string | null>(null);
+  const [ftpURL, setftpURL] = useState<string | null>(null);
   useEffect(() => {
     async function loadSession() { setSession(await db.sessions.get(parseInt(localStorage.getItem("activeSession")!))); }
     loadSession();
   }, []);
+  useEffect(() => {
+    if (!session) return;
+    query(session, "providers").then((res) => {
+      if ("error" in res) {
+        alert(res.error);
+        return;
+      }
+      setWebdavURL(res.body.webdav);
+      setftpURL(res.body.ftp);
+    });
+  }, [session]);
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-center text-2xl font-semibold">Welcome{session ? `, ${session.username}` : ""}</h1>
+      <div className="flex flex-col gap-2 items-center">
+        {webdavURL && <div className="flex flex-col text-center gap-2 items-center p-2 bg-slate-600 rounded-lg w-fit">
+          <h1 className="text-lg font-semibold">WebDAV</h1>
+        </div>}
+      </div>
     </div>
   )
 }
---
+++ web/utils/api.ts
@@ -0,0 +1,16 @@
+import { Session } from "@/folderharborweb";
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function query(session: Session, path: string, init?: RequestInit): Promise<{ error: string } | { body: any }> {
+  let body;
+  const url = new URL(session.server);
+  url.pathname += path;
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", `Bearer ${session.token}`);
+  try {
+    const res = await fetch(url.toString(), { ...init, headers });
+    body = await res.json();
+  } catch (err) { return { error: `Error: ${err}` } }
+  if (body.error) return { error: `Error (${body.error}): ${body.message}` }
+  return { body };
+}
