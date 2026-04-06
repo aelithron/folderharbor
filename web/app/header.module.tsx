5c0d0d7a3a0b908ecf7dd3c70704a6b8e960cc82
Nova <aelithron@gmail.com>
2026-04-06 14:57:09 -0600

    web: make a header and small user settings menu

---
+++ web/app/header.module.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { Session } from "@/folderharborweb";
+import { db } from "@/utils/db";
+import { faSignOut, faSync, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import logo from "@/public/logo.webp";
+import Link from "next/link";
+import query from "@/utils/api";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+  const router = useRouter();
+  const [open, setOpen] = useState<boolean>(false);
+  const [session, setSession] = useState<Session | undefined>();
+  useEffect(() => {
+    async function loadSession() { setSession(await db.sessions.get(parseInt(localStorage.getItem("activeSession")!))); }
+    loadSession();
+  }, []);
+  async function signOut() {
+    const res = await query(session!, "auth", { method: "DELETE" });
+    if ("error" in res) {
+      alert(res.error);
+      return;
+    }
+    await db.sessions.delete(session!.webID);
+    localStorage.removeItem("activeSession");
+    router.push("/");
+  }
+  return (
+    <header className="flex z-25 sticky justify-between gap-2 p-2 md:px-8 bg-violet-700/40 w-full align-middle items-center">
+      <Link href={"/home"} className="text-lg flex items-center align-middle font-semibold hover:text-sky-500"><Image src={logo} alt="FolderHarbor Logo" loading="eager" height={50} width={50} /> FolderHarbor</Link>
+      <div className="relative">
+        <button onClick={() => setOpen(!open)} className="hover:text-sky-500"><FontAwesomeIcon icon={faUser} size="lg" /></button>
+        {open && <div className="absolute right-0 mt-4 flex flex-col p-2 rounded-lg bg-violet-700/40 z-50 w-max text-start items-center">
+          <h1 className="text-lg font-semibold text-center">Welcome{session?.username ? `, ${session!.username}!` : "!"}</h1>
+          <Link href={"/"} className="hover:text-sky-500"><FontAwesomeIcon icon={faSync} /> Switch Account</Link>
+          <button onClick={() => signOut()} className="hover:text-sky-500"><FontAwesomeIcon icon={faSignOut} /> Sign Out</button>
+        </div>}
+      </div>
+    </header>
+  )
+}
--- web/app/home/page.tsx
+++ web/app/home/page.tsx
@@ -1,9 +1,13 @@
+import Header from "../header.module";
 import Home from "./home.module";
 
 export default function Page() {
   return (
-    <main className="flex flex-col p-8 md:p-20 min-h-screen">
-      <Home />
+    <main>
+      <Header />
+      <div className="flex flex-col p-8 md:p-20 min-h-screen">
+        <Home />
+      </div>
     </main>
   );
 }
