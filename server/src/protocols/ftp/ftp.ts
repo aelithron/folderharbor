78713f9ea8306d4ff73450a1a8877ec4f2734121
Nova <aelithron@gmail.com>
2026-04-14 00:09:25 -0600

    server: fix mac pasv ftp, fix config, start on metadata filter for ftp

--- server/src/protocols/ftp/ftp.ts
+++ server/src/protocols/ftp/ftp.ts
@@ -1,10 +1,7 @@
 import { FtpSrv } from "ftp-srv";
 import { ftpAuth } from "./helpers.js";
-import { networkInterfaces } from "os";
-import { Netmask } from "netmask";
 import type z from "zod";
 import type { Config } from "../../utils/config.js";
-import { getConfig } from "../../index.js";
 
 export default async function startFTP(config: z.infer<typeof Config>, sslKey?: string, sslCert?: string): Promise<FtpSrv> {
   let server;
@@ -28,8 +25,5 @@ export default async function startFTP(config: z.infer<typeof Config>, sslKey?: 
 }
 function pasvResolver(address: string): string {
   if (address === "127.0.0.1" || address === "::1") return "127.0.0.1";
-  const networks: { [key: string]: string } = {};
-  for (const name of Object.keys(networkInterfaces())) for (const net of (networkInterfaces()!)[name]!) if (net.family === 'IPv4' && !net.internal) networks[net.address + "/24"] = net.address;
-  for (const network in networks) if (new Netmask(network).contains(address) && networks[network] !== undefined) return networks[network];
-  return getConfig()?.ftp.pasv.address || "127.0.0.1";
+  return address;
 }
