e348af8fe60079dad2f0e7ac0229ba4df2361433
Nova <aelithron@gmail.com>
2026-04-18 00:18:33 -0600

    cli: fix config issue on windows file path was wrong for windows, should work now

--- cli/main.go
+++ cli/main.go
@@ -11,15 +11,15 @@ import (
 )
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil { panic (err) }
 	viper.SetConfigName("config")
 	viper.SetConfigType("json")
-	viper.AddConfigPath("$HOME/.folderharbor")
+	viper.AddConfigPath(filepath.Join(homeDir, ".folderharbor"))
 	var fileLookupErr viper.ConfigFileNotFoundError
 	if err := viper.ReadInConfig(); err != nil {
 		if errors.As(err, &fileLookupErr) {
 			fmt.Println("Welcome to FolderHarbor CLI! Creating a config file...")
-			homeDir, err := os.UserHomeDir()
-			if err != nil { panic (err) }
 			if err := os.Mkdir(filepath.Join(homeDir, ".folderharbor"), os.ModePerm); err != nil { panic (err) }
 			if err := viper.SafeWriteConfig(); err != nil { panic (err) }
 		} else { panic (err) }
