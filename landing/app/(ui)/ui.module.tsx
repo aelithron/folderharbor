6c80faf7ab6c27382574636031103fc563a43c4c
Nova <aelithron@gmail.com>
2026-04-19 14:28:38 -0600

    landing: improve styles, add image

--- landing/app/(ui)/ui.module.tsx
+++ landing/app/(ui)/ui.module.tsx
@@ -6,13 +6,13 @@ import { faGlobe, faPlay, faServer, faTerminal } from "@fortawesome/free-solid-s
 
 export function Header() {
   return (
-    <header className="flex gap-4 bg-violet-700/40 p-4 md:px-8 justify-between align-middle rounded-xl m-4">
-      <Link href={"/"}><Image src={logo} alt="The FolderHarbor logo" loading="eager" className="w-1/8" /></Link>
+    <header className="flex gap-4 bg-violet-700/40 p-2 md:py-4 md:px-8 justify-between items-center align-middle rounded-xl m-4">
+      <Link href={"/"}><Image src={logo} alt="The FolderHarbor logo" loading="eager" className="xs:w-3/4 sm:w-1/4 md:w-1/8" /></Link>
       <div className="flex gap-3 align-middle items-center">
-        <Link href={"/server"} className="hover:text-sky-500"><FontAwesomeIcon icon={faServer} /> Server</Link>
-        <Link href={"/cli"} className="hover:text-sky-500"><FontAwesomeIcon icon={faTerminal} /> CLI</Link>
-        <Link href={"/web"} className="hover:text-sky-500"><FontAwesomeIcon icon={faGlobe} /> Web Panel</Link>
-        <Link href={"/demo"} className="hover:text-sky-500 bg-violet-500/40 py-1 px-2 rounded-xl"><FontAwesomeIcon icon={faPlay} /> Demo</Link>
+        <Link href={"/server"} className="hover:text-sky-500 flex items-center gap-1"><FontAwesomeIcon icon={faServer} /> Server</Link>
+        <Link href={"/cli"} className="hover:text-sky-500 flex items-center gap-1"><FontAwesomeIcon icon={faTerminal} /> CLI</Link>
+        <Link href={"/web"} className="hover:text-sky-500 flex items-center gap-1"><FontAwesomeIcon icon={faGlobe} /> Web</Link>
+        <Link href={"/demo"} className="hover:text-sky-500 bg-violet-500/40 py-1 px-2 rounded-xl flex items-center gap-1"><FontAwesomeIcon icon={faPlay} /> Demo</Link>
       </div>
     </header>
   );
--- landing/app/web/page.tsx
+++ landing/app/web/page.tsx
@@ -1,16 +1,19 @@
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
+import webPhoto from "@/public/shots/web.webp"
+import Image from "next/image";
 
 export const metadata: Metadata = { title: "Web" }
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-8 md:px-16">
-      <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-2">
         <div className="flex flex-col gap-2 items-center justify-center text-center">
-          <h1 className="text-3xl font-semibold"><FontAwesomeIcon icon={faGlobe} /> FolderHarbor Web</h1>
-          <p>A web panel to interact with FolderHarbor servers.</p>
+          <h1 className="text-4xl font-semibold"><FontAwesomeIcon icon={faGlobe} /> FolderHarbor Web</h1>
+          <p className="text-lg">A web panel to interact with FolderHarbor servers.</p>
         </div>
+        <div className="flex justify-center md:col-span-2"><Image src={webPhoto} alt="A stylized screenshot of the FolderHarbor web panel." loading="eager" className="rounded-xl md:w-3/4" /></div>
       </div>
     </main>
   );
