3de0265e77496f30dfaef054dd7478a1b461f19c
Nova <aelithron@gmail.com>
2026-04-19 04:03:22 -0600

    landing: improve image loading and add favicon

--- landing/app/(ui)/ui.module.tsx
+++ landing/app/(ui)/ui.module.tsx
@@ -7,7 +7,7 @@ import { faGlobe, faPlay, faServer, faTerminal } from "@fortawesome/free-solid-s
 export function Header() {
   return (
     <header className="flex gap-4 bg-violet-700/40 p-4 md:px-8 justify-between align-middle rounded-xl m-4">
-      <Link href={"/"}><Image src={logo} alt="The FolderHarbor logo" width={80} height={80} /></Link>
+      <Link href={"/"}><Image src={logo} alt="The FolderHarbor logo" loading="eager" className="w-1/8" /></Link>
       <div className="flex gap-3 align-middle items-center">
         <Link href={"/server"} className="hover:text-sky-500"><FontAwesomeIcon icon={faServer} /> Server</Link>
         <Link href={"/cli"} className="hover:text-sky-500"><FontAwesomeIcon icon={faTerminal} /> CLI</Link>
--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import logo from "@/public/logo.webp";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8 md:px-16">
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="flex flex-col gap-2 items-center justify-center text-center">
-          <Image src={logo} alt="The FolderHarbor logo" width={300} height={300} />
+          <Image src={logo} alt="The FolderHarbor logo" loading="eager" className="w-3/4" />
           <h1 className="text-3xl font-semibold">FolderHarbor</h1>
           <p>A powerful, multi-protocol file server with RBAC.</p>
+          <Link href={"/demo"} className="bg-violet-500 p-1 rounded-xl text-lg hover:text-sky-500"><FontAwesomeIcon icon={faPlay} /> Try a Demo</Link>
         </div>
       </div>
       
