0379ca706913a516b0846bb7445bdb5f080c4099
Nova <aelithron@gmail.com>
2026-04-19 19:17:38 -0600

    landing: finish cli download code

--- landing/app/cli/page.tsx
+++ landing/app/cli/page.tsx
@@ -1,31 +1,97 @@
-import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { faTerminal, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
+import { headers } from "next/headers";
 import Image from "next/image";
 import cliPhoto from "@/public/shots/cli.webp";
-import DownloadCLI from "./download.module";
 
 export const metadata: Metadata = { title: "CLI" }
 export default async function Page() {
-  const version = await getVersion();
+
   return (
     <main className="flex flex-col min-h-screen p-8 md:px-16">
       <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-2">
         <div className="flex flex-col gap-2 items-center justify-center text-center">
           <h1 className="text-4xl font-semibold"><FontAwesomeIcon icon={faTerminal} /> FolderHarbor CLI</h1>
           <p className="text-lg">A powerful CLI to interact with FolderHarbor servers.</p>
-          <DownloadCLI version={version} />
+          <DownloadCLI />
         </div>
         <div className="flex justify-center md:col-span-2"><Image src={cliPhoto} alt="A stylized screenshot of the FolderHarbor web panel." loading="eager" className="rounded-xl md:w-3/4" /></div>
       </div>
     </main>
   );
 }
-async function getVersion(): Promise<{ tag: string, name: string } | null> {
+async function DownloadCLI() {
+  const userAgent = (await headers()).get("user-agent");
+  let os: "macOS" | "linux" | "windows" | "other";
+  let version: { tag: string, name: string };
   try {
-    const res = await fetch("https://api.github.com/repos/aelithron/folderharbor/releases");
+    const res = await fetch("https://api.github.com/repos/aelithron/folderharbor/releases", { headers: { "User-Agent": "Aelithron-FolderHarbor-LandingPage", "Authorization": `Bearer ${process.env.GITHUB_PAT!}` } });
     const tag = (await res.json() as { tag_name: string, name: string }[]).find((item) => item.tag_name.startsWith("cli/"));
-    if (!tag) return null;
-    return { name: tag.name, tag: tag.tag_name.split("/")[1] }
-  } catch { return null; }
+    if (!tag) return false;
+    version = { name: tag.name, tag: tag.tag_name.split("/")[1] }
+  } catch { return false; }
+  if (userAgent) {
+    if (userAgent.includes("Macintosh")) {
+      os = "macOS"
+    } else if (userAgent.includes("Win")) {
+      os = "windows";
+    } else if (userAgent.includes("iPhone") || userAgent.includes("Android")) {
+      os = "other";
+    } else os = "linux";
+  } else os = "other";
+  const base = `https://github.com/aelithron/folderharbor/releases/download/cli/${version.tag}/folderharbor-cli-${version.tag}`;
+  return (
+    <div className="flex flex-col gap-2 bg-slate-700 p-3 rounded-xl mt-4">
+      <h1 className="text-lg font-semibold">Download {version.name}</h1>
+      {os === "macOS" && <div>
+        <div className="grid grid-cols-2 gap-2">
+          <a href={`${base}-macos-arm64`} target="_blank" className="bg-violet-500 p-1 px-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Apple Silicon</a>
+          <a href={`${base}-macos-amd64`} target="_blank" className="bg-violet-500 p-1 px-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Intel Macs</a>
+        </div>
+        <div className="flex gap-2 mt-2 items-center">
+          <p>Or, download for:</p>
+          <a href={`${base}-linux-amd64`} target="_blank" className="hover:text-sky-500">Linux x86_64</a>
+          |
+          <a href={`${base}-linux-arm64`} target="_blank" className="hover:text-sky-500">Linux ARM64</a>
+          |
+          <a href={`${base}-windows-amd64.exe`} target="_blank" className="hover:text-sky-500">Windows</a>
+        </div>
+      </div>}
+      {os === "windows" && <div>
+        <a href={`${base}-windows-amd64.exe`} target="_blank" className="bg-violet-500 p-1 px-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Windows</a>
+        <div className="flex gap-2 mt-2 items-center">
+          <p>Or, download for:</p>
+          <a href={`${base}-macos-arm64`} target="_blank" className="hover:text-sky-500">Apple Silicon Macs</a>
+          |
+          <a href={`${base}-macos-amd64`} target="_blank" className="hover:text-sky-500">Intel Macs</a>
+          |
+          <a href={`${base}-linux-amd64`} target="_blank" className="hover:text-sky-500">Linux x86_64</a>
+          |
+          <a href={`${base}-linux-arm64`} target="_blank" className="hover:text-sky-500">Linux ARM64</a>
+        </div>
+      </div>}
+      {os === "linux" && <div>
+        <div className="grid grid-cols-2 gap-2">
+          <a href={`${base}-linux-amd64`} target="_blank" className="bg-violet-500 p-1 px-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Linux x86_64</a>
+          <a href={`${base}-linux-arm64`} target="_blank" className="bg-violet-500 p-1 px-2 rounded-xl hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Linux ARM64</a>
+        </div>
+        <div className="flex gap-2 mt-2 items-center">
+          <p>Or, download for:</p>
+          <a href={`${base}-macos-arm64`} target="_blank" className="hover:text-sky-500">Apple Silicon Macs</a>
+          |
+          <a href={`${base}-macos-amd64`} target="_blank" className="hover:text-sky-500">Intel Macs</a>
+          |
+          <a href={`${base}-windows-amd64.exe`} target="_blank" className="hover:text-sky-500">Windows</a>
+        </div>
+      </div>}
+      {os === "other" && <div className="flex flex-col gap-2 mt-2 text-start">
+        <a href={`${base}-macos-arm64`} target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Apple Silicon Macs</a>
+        <a href={`${base}-macos-amd64`} target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Intel Macs</a>
+        <a href={`${base}-linux-amd64`} target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Linux x86_64</a>
+        <a href={`${base}-linux-arm64`} target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Linux ARM64</a>
+        <a href={`${base}-windows-amd64.exe`} target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faDownload} /> Windows</a>
+      </div>}
+    </div>
+  )
 }
--- landing/next.config.ts
+++ landing/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: "standalone",
+  allowedDevOrigins: ["192.168.5.1"]
 };
 
 export default nextConfig;
