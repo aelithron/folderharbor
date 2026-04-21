d7b9d3e4cd03f40ebe6cda48367cd197e69c5e9e
Nova <aelithron@gmail.com>
2026-04-20 23:40:08 -0600

    cli: make people press enter to close this is to make sure people don't just double-click the binary and see a terminal show for half a second before exiting

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,14 +1,25 @@
 package cmd
+
 import (
-	"github.com/spf13/cobra"
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 var rootCMD = &cobra.Command{
 	Use: "folderharbor",
 	Short: "a cli for interacting with folderharbor servers",
 	Long: "a powerful cli for file management and admin actions, used with folderharbor servers",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+		if (term.IsTerminal(int(os.Stdin.Fd()))) {
+			fmt.Println()
+			fmt.Println("Press Enter to exit...")
+			fmt.Scanln();
+		}
+	},
 }
 func Execute() {
 	if err := rootCMD.Execute(); err != nil {
