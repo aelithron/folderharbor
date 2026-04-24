def24fad86366f84c012f6c5387cb6bfad7e3f13
Nova <aelithron@gmail.com>
2026-04-23 19:06:58 -0600

    server: work more on install script, nearly finished!

--- server/install.sh
+++ server/install.sh
@@ -16,5 +16,38 @@ case "$(uname -m)" in
     ;;
 esac
 echo "Finding latest server version..."
-LATEST=$(curl -fs -A "https://fh.novatea.dev/api/serverversion")
-echo "$LATEST"
+LATEST=$(curl -fsS https://fh.novatea.dev/api/version/server)
+if [ -z "$LATEST" ]; then
+  echo "Error: Couldn't find a FolderHarbor Server version! Please make a GitHub Issue on https://github.com/aelithron/folderharbor/issues."
+  exit 1
+fi
+echo "Found FolderHarbor Server $LATEST!"
+echo "Beginning installation..."
+mkdir -p /usr/lib/folderharbor
+mkdir -p /etc/folderharbor
+cd /usr/lib/folderharbor
+echo "Creating user..."
+useradd -r -s /usr/sbin/nologin folderharbor
+echo "Downloading..."
+curl -A "Aelithron-FolderHarbor-Installer" -o "/usr/lib/folderharbor/folderharbor.tar.gz" -fsSL "https://github.com/aelithron/folderharbor/releases/download/server%2F$LATEST/folderharbor-server-$LATEST-linux-$ARCH.tar.gz"
+echo "Extracting..."
+tar -xzf /usr/lib/folderharbor/folderharbor.tar.gz
+chmod +x folderharbor-server
+chmod 600 example.config.json
+chmod -R 755 drizzle
+chmod -R 755 node_modules
+chmod 644 index.js
+chmod 644 folderharbor.service
+chmod 755 folderharbor-server
+chown -R folderharbor /usr/lib/folderharbor
+echo "Setting up some things..."
+mv folderharbor.service /etc/systemd/system/folderharbor.service
+mv folderharbor-server /usr/bin/folderharbor-server
+systemctl daemon-reload
+echo "Installed FolderHarbor Server $LATEST!"
+read -p "Do you want to install the CLI as well? (y/n): " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+fi
+echo "Your server is ready to be used.
